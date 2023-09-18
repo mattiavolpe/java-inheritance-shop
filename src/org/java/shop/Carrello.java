fac9dab32ac949609af48dee41b81c96aabf064a
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-09-18 17:34:30 +0200

    ADD cart total output

--- src/org/java/shop/Carrello.java
+++ src/org/java/shop/Carrello.java
@@ -3,8 +3,12 @@ package org.java.shop;
 import java.util.Scanner;
 
 public class Carrello {
-	public static void main(String[] args) {
-		String productType = "0";
+	public static void main(String[] args) {		
+		String productType = null;
+		Prodotto[] products = new Prodotto[1];
+		
+		float cartTotal = 0;
+		
 		Scanner sc = new Scanner(System.in);
 		
 		do {
@@ -20,63 +24,91 @@ public class Carrello {
 				productType = sc.nextLine();
 			} while (!(productType.equals("0") || productType.equals("1") || productType.equals("2") || productType.equals("3")));
 			
-			if (productType.equals("0")) {
-				sc.close();
-				return;
-			}
-			
-			System.out.print("Type the product code: ");
-			String code = sc.nextLine();
-			
-			System.out.print("Type the product name: ");
-			String name = sc.nextLine();
-			
-			System.out.print("Type the product brand: ");
-			String brand = sc.nextLine();
-			
-			System.out.print("Type the basic price: ");
-			String price = sc.nextLine();
-			
-			System.out.print("Type the VAT percentage: ");
-			String vat = sc.nextLine();
-			
-			switch (productType) {
-		 		case "1":
-		 			System.out.print("Type the IMEI: ");
-		 			String imei = sc.nextLine();
-		 			System.out.print("Type the memory amount in GB: ");
-		 			String memory = sc.nextLine();
-		 			System.out.println("\n\n");
-		 			Smartphone smartphone = new Smartphone(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), imei, Short.parseShort(memory));
-		 			break;
-		 		case "2":
-		 			System.out.print("Type the inches: ");
-		 			String inches = sc.nextLine();
-		 			String isSmartAnswer = null;
-		 			do {
-		 				System.out.print("Is the TV smart? [y] yes --- [n] no: ");
-		 				isSmartAnswer = sc.nextLine().toLowerCase();
-		 			} while (!(isSmartAnswer.equals("y") || isSmartAnswer.equals("n")));
-		 			boolean isSmart = isSmartAnswer.equals("y") ? true : false;
-		 			System.out.println("\n\n");
-		 			Televisore tv = new Televisore(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), Short.parseShort(inches), isSmart);
-		 			break;
-	 			default:
-	 				System.out.print("Type the color: ");
-		 			String color = sc.nextLine();
-		 			String isWirelessAnswer = null;
-		 			do {
-		 				System.out.print("Is the headphone wireless? [y] yes --- [n] no: ");
-		 				isWirelessAnswer = sc.nextLine().toLowerCase();
-		 			} while (!(isWirelessAnswer.equals("y") || isWirelessAnswer.equals("n")));
-		 			boolean isWireless = isWirelessAnswer.equals("y") ? true : false;
-		 			System.out.println("\n\n");
-		 			Cuffia headphone = new Cuffia(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), color, isWireless);
-	 				break;
+			if (!productType.equals("0")) {
+				Prodotto currentItem = null;
+				
+				System.out.print("Type the product code: ");
+				String code = sc.nextLine();
+				
+				System.out.print("Type the product name: ");
+				String name = sc.nextLine();
+				
+				System.out.print("Type the product brand: ");
+				String brand = sc.nextLine();
+				
+				System.out.print("Type the basic price: ");
+				String price = sc.nextLine();
+				
+				System.out.print("Type the VAT percentage: ");
+				String vat = sc.nextLine();
+				
+				switch (productType) {
+			 		case "1":
+			 			System.out.print("Type the IMEI: ");
+			 			String imei = sc.nextLine();
+			 			System.out.print("Type the memory amount in GB: ");
+			 			String memory = sc.nextLine();
+			 			System.out.println("\n\n");
+			 			Smartphone smartphone = new Smartphone(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), imei, Short.parseShort(memory));
+			 			currentItem = smartphone;
+			 			break;
+			 		case "2":
+			 			System.out.print("Type the inches: ");
+			 			String inches = sc.nextLine();
+			 			String isSmartAnswer = null;
+			 			do {
+			 				System.out.print("Is the TV smart? [y] yes --- [n] no: ");
+			 				isSmartAnswer = sc.nextLine().toLowerCase();
+			 			} while (!(isSmartAnswer.equals("y") || isSmartAnswer.equals("n")));
+			 			boolean isSmart = isSmartAnswer.equals("y") ? true : false;
+			 			System.out.println("\n\n");
+			 			Televisore tv = new Televisore(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), Short.parseShort(inches), isSmart);
+			 			currentItem = tv;
+			 			break;
+		 			default:
+		 				System.out.print("Type the color: ");
+			 			String color = sc.nextLine();
+			 			String isWirelessAnswer = null;
+			 			do {
+			 				System.out.print("Is the headphone wireless? [y] yes --- [n] no: ");
+			 				isWirelessAnswer = sc.nextLine().toLowerCase();
+			 			} while (!(isWirelessAnswer.equals("y") || isWirelessAnswer.equals("n")));
+			 			boolean isWireless = isWirelessAnswer.equals("y") ? true : false;
+			 			System.out.println("\n\n");
+			 			Cuffia headphone = new Cuffia(code, name, brand, Float.parseFloat(price), Byte.parseByte(vat), color, isWireless);
+			 			currentItem = headphone;
+		 				break;
+				}
+				
+				if (products[0] == null) {
+					products[0] = currentItem;
+				} else {
+					Prodotto[] tempProducts = new Prodotto[products.length];
+					
+					for (int i = 0; i < products.length; i++) {
+						tempProducts[i] = products[i];
+					}
+					
+					products = new Prodotto[tempProducts.length + 1];
+					
+					for (int i = 0; i < tempProducts.length; i++) {
+						products[i] = tempProducts[i];
+					}
+					
+					products[products.length - 1] = currentItem;
+				}
+				
+				cartTotal += Float.parseFloat(products[products.length - 1].getFullPrice().replace(',', '.'));
 			}
 		} while (!productType.equals("0"));
 		
-		sc.close();
+		for (int i = 0; i < products.length; i++) {
+			System.out.println("\n\n");
+			System.out.println(products[i]);
+		}
 		
+		System.out.println("\n\nCart total: " + String.format("%.02f", cartTotal) + "€");
+		
+		sc.close();
 	}
 }
